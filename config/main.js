5323774ebdd2275e603113ab961c2876ed03528d
forkerforsites <ronald.turner.954@k12.friscoisd.org>
2025-01-21 17:20:55 -0600

    main.js
    site name

--- config/main.js
+++ config/main.js
@@ -1,7 +1,7 @@
 // This changes the title of your site
 
-var sitename = "native."; // Change this to change the name of your website.
-var subtext = "v1.2"; // set the subtext
+var sitename = "Ronnie's Games"; // Change this to change the name of your website.
+var subtext = "Ronnie's Games"; // set the subtext
 
 // more settings in main.css
 
